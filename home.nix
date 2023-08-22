837a773370b9dcca9f84afa90cb6fee2c2e726fb
Sylvain Carisey <sylvain@carisey.dev>
2023-08-22 09:03:19 +0200

    feat(tmux):vi moves

--- home.nix
+++ home.nix
@@ -144,6 +144,18 @@ in {
     mouse = true;
     extraConfig = ''
       source-file "$HOME/.gruvbox.tmuxtheme"
+      # vim-like pane resizing  
+      bind -r C-k resize-pane -U
+      bind -r C-j resize-pane -D
+      bind -r C-h resize-pane -L
+      bind -r C-l resize-pane -R
+      # vim-like pane switching
+      bind -r k select-pane -U 
+      bind -r j select-pane -D 
+      bind -r h select-pane -L 
+      bind -r l select-pane -R 
+
+      #vi selection mode
       setw -g mode-keys vi
       set-option -g status-interval 5
       set-option -g automatic-rename on
